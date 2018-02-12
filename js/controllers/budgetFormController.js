562d886f6287c9789e84e66673ae7bf04e8c1a7a
Schlichting <schl0159@STTHOMAS.EDU>
2018-02-12 09:56:57 -0600

    repeat test

--- js/controllers/budgetFormController.js
+++ js/controllers/budgetFormController.js
@@ -1,18 +1,6 @@
 function budgetFormControllerFunction($scope) {
 
-  $scope.init = function (){
-    console.log("Version: 0.0.1")
-    console.log("init budget list")
-  }
-
-
-  $scope.budgets = null;
-
-
-  $scope.loadBudgets = function (){
-    //todo
-    $scope.budgets = budgets;
-  }
+  
 
 }
 
--- js/controllers/budgetListController.js
+++ js/controllers/budgetListController.js
@@ -1,5 +1,20 @@
 function budgetListControllerFunction($scope) {
 
+  $scope.init = function (){
+    console.log("Version: 0.0.1")
+    console.log("init budget list")
+  }
+
+
+  $scope.budgets = null;
+
+
+  $scope.loadBudgets = function (){
+    //todo
+    $scope.budgets = budgets;
+  }
+
+
 }
 
 budgetApp.controller('budgetListCtrl', budgetListControllerFunction);
