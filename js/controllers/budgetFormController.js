7a85ef64e7a362ca624cb4fd142d13aa9d52d993
Schlichting <schl0159@stthomas.edu>
2018-02-12 09:53:44 -0600

    repeat test

--- js/controllers/budgetFormController.js
+++ js/controllers/budgetFormController.js
@@ -1,5 +1,13 @@
 function budgetFormControllerFunction($scope) {
 
+  $scope.budgets = null;
+
+
+  $scope.loadBudgets = function (){
+    //todo
+    $scope.budgets = budgets;
+  }
+
 }
 
 budgetApp.controller('budgetFormCtrl', budgetFormControllerFunction);
--- js/mock-database.js
+++ js/mock-database.js
@@ -1,6 +1,5 @@
 var budgets = [
   {name:"budget1", fields: ["test1", "test2"], values: [100, 200]}
 
-
-
+  
 ];
